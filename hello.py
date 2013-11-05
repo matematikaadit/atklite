00859926728f2c254b434797b917f3c2daa788fa
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-11-05 17:07:20 +0700

    templating

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
-import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_name(name="World"):
+    return render_template('hello.html', name=name)
