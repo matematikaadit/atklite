a8753f6d55efb628446f132fecfffe671ebfe40b
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-02 06:00:19 +0700

    first attempt to fix #14

--- atklite.py
+++ atklite.py
@@ -7,12 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     items = get_items()
-    today = date.today().strftime("%A %B %d, %Y")
+    t = date.today()
+    today = "{0:%A} {0:%B} {1}, {0:%Y}".format(t,ordinal(t.day))
     # %A: Weekday full: Saturday
     # %B: Month full: November
     # %d: day: 30
     # %Y: full year: 2013
     return render_template('index.html', items=items, today=today)
+
+def ordinal(day):
+    if 4 <= day <= 20 or 24 <= day <= 30:
+        suffix = 'th'
+    else:
+        suffix = ['st','nd','rd'][day % 10 - 1]
+    return str(day)+suffix
 
 @app.route('/item/<urlname>')
 def item(urlname=None):
